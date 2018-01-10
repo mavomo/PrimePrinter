bc7ca903244e8db495333c49a5b0150293bbf4d7
mavomo <michelle.avomo@gmail.com>
2018-01-10 01:06:47 +0100

    init js version

--- javascript/src/primeprinter.js
+++ javascript/src/primeprinter.js
@@ -1,73 +1,66 @@
 class PrimePrinter {
 	
 	static print() {
-		let finIteration = 1000;
-		let numberItemByPage = 50;
-		let numberColum = 4;
-		let length = 30;
-		let P = new Array(finIteration + 1);
+		let M = 1000;
+		let RR = 50;
+		let CC = 4;
+		let ORDMAX = 30;
+		let P = new Array(M + 1);
 		let PAGENUMBER;
 		let PAGEOFFSET;
 		let ROWOFFSET;
 		let C;
 		let J;
-		let Iteration;
+		let K;
 		let JPRIME;
 		let ORD;
 		let SQUARE;
 		let N = 0;
-		let MULT = new Array(length + 1);
-		let primesToPrint = "";
+		let MULT = new Array(ORDMAX + 1);
 		J = 1;
-		Iteration = 1;
+		K = 1;
 		P[1] = 2;
 		ORD = 2;
 		SQUARE = 9;
-		
-		let J2 = J;
-		let SQUARE2 = SQUARE;
-		let N2 = N;
-		let Iteration2 = Iteration;
-		let ORD2 = ORD;
-		let JPRIME2 = JPRIME;
-		while (Iteration2 < finIteration) {
+		let text = "";
+		while (K < M) {
 			do {
-				J2 = J2 + 2; //3
-				if (J2 == SQUARE2) { // J == 9
-					ORD2 = ORD2 + 1; //3
-					SQUARE2 = P[ORD2] * P[ORD2];
-					MULT[ORD2 - 1] = J2;
+				J = J + 2;
+				if (J == SQUARE) {
+					ORD = ORD + 1;
+					SQUARE = P[ORD] * P[ORD];
+					MULT[ORD - 1] = J;
 				}
-				N2 = 2;
-				JPRIME2 = true;
-				while (N2 < ORD2 && JPRIME2) {
-					while (MULT[N2] < J2)
-						MULT[N2] = MULT[N2] + P[N2] + P[N2];
-					if (MULT[N2] == J2)
-						JPRIME2 = false;
-					N2 = N2 + 1;
+				N = 2;
+				JPRIME = true;
+				while (N < ORD && JPRIME) {
+					while (MULT[N] < J)
+						MULT[N] = MULT[N] + P[N] + P[N];
+					if (MULT[N] == J)
+						JPRIME = false;
+					N = N + 1;
 				}
-			} while (!JPRIME2);
-			Iteration2 = Iteration2 + 1;
-			P[Iteration2] = J2;
+			} while (!JPRIME);
+			K = K + 1;
+			P[K] = J;
 		}
 		PAGENUMBER = 1;
 		PAGEOFFSET = 1;
 		
-		while (PAGEOFFSET <= finIteration) {
-			primesToPrint += "The First " + finIteration + " Prime numbers --- Page " + PAGENUMBER + " \n";
-			for (ROWOFFSET = PAGEOFFSET; ROWOFFSET < PAGEOFFSET + numberItemByPage; ROWOFFSET++) {
-				for (C = 0; C < numberColum; C++)
-					if (ROWOFFSET + C * numberItemByPage <= finIteration)
-						primesToPrint += P[ROWOFFSET + C * numberItemByPage] + " ";
-				primesToPrint += "\n";
+		while (PAGEOFFSET <= M) {
+			text += "The First " + M + " Prime numbers --- Page " + PAGENUMBER + " \n";
+			for (ROWOFFSET = PAGEOFFSET; ROWOFFSET < PAGEOFFSET + RR; ROWOFFSET++) {
+				for (C = 0; C < CC; C++)
+					if (ROWOFFSET + C * RR <= M)
+						text += P[ROWOFFSET + C * RR] + " ";
+				text += "\n";
 			}
-			primesToPrint += '\f';
+			text += '\f';
 			PAGENUMBER = PAGENUMBER + 1;
-			PAGEOFFSET = PAGEOFFSET + numberItemByPage * numberColum;
+			PAGEOFFSET = PAGEOFFSET + RR * CC;
 		}
-		console.log(primesToPrint);
-		return primesToPrint;
+		console.log(text);
+		return text;
 	}
 }
 
