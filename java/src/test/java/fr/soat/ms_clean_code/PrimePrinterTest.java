893ada8d1b1e284ee022f899a36b907b0eb1ef23
snamdar <sepehr.namdar@ceetiz.com>
2018-09-28 14:52:28 +0200

    test correction

--- java/src/test/java/fr/soat/ms_clean_code/PrimePrinterTest.java
+++ java/src/test/java/fr/soat/ms_clean_code/PrimePrinterTest.java
@@ -1,6 +1,7 @@
 package fr.soat.ms_clean_code;
 
-import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,32 +9,32 @@ import java.io.*;
 
 public class PrimePrinterTest {
 
-    private String leadFile;
+    private PrintStream out;
 
     @Before
-    public void setup() throws Exception {
-        generateLeadFile();
+    public void setup() throws FileNotFoundException {
+        out = System.out;
+        System.setOut(new PrintStream(new FileOutputStream("lead.txt")));
+    }
+
+    @After
+    public void teardown() {
+        System.setOut(out);
+        new File("lead.txt").delete();
     }
 
     @Test
     public void makeSureOutputMatchesGold() throws Exception {
-        fr.soat.ms_clean_code.PrimePrinter.main(new String[0]);
-        BufferedReader lead = new BufferedReader(new FileReader(leadFile));
+        PrimePrinter.main(new String[0]);
+        BufferedReader lead = new BufferedReader(new FileReader("lead.txt"));
+        BufferedReader gold = new BufferedReader(new FileReader("gold.txt"));
 
-        String goldFilePath = new File("gold.txt").getAbsolutePath();
-        BufferedReader gold = new BufferedReader(new FileReader(goldFilePath));
         String line;
-        while ((line= gold.readLine()) != null)
-            Assertions.assertThat(line).isEqualTo(lead.readLine());
+        while ((line = gold.readLine()) != null) {
+            Assert.assertEquals(line, lead.readLine());
+        }
 
-        Assertions.assertThat(lead.readLine()).isNull();
-
-    }
-
-    private void generateLeadFile() throws FileNotFoundException {
-        leadFile = new File("lead.txt").getAbsolutePath();
-        FileOutputStream fileOutputStream = new FileOutputStream(leadFile);
-        System.setOut(new PrintStream(fileOutputStream));
+        Assert.assertNull(lead.readLine());
     }
 
 
